0f8c597de6c16ae3b2c8769d29e8e76fdc59f3c0
Mia Baker <mia77baker@gmail.com>
2019-10-17 17:03:32 -0700

    drew art

--- CS160/processing/first_project/sketch.js
+++ CS160/processing/first_project/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
 }
 
 function draw() {
-  // put drawing code here
+  c = color(35, 5, 80);
+  background(c);
+  fill(255,255,255);
+  ellipse(200,200,50,50);
+  ellipse (200, 310, 40, 150);
+  ellipse (200, 90, 40, 150);
+  ellipse (310, 200, 150, 40);
+  ellipse (90, 200, 150, 40);
 }
