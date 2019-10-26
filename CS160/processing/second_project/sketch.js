2e8a4d615677ef3307a286c506a8964d2b7f8c80
Mia Baker <mia77baker@gmail.com>
2019-10-26 16:07:18 -0700

    js sketches

--- CS160/processing/second_project/sketch.js
+++ CS160/processing/second_project/sketch.js
@@ -1,13 +1,32 @@
+let x = 100,
+  y = 100,
+  angle1 = 0.0,
+  segLength = 60;
+
 function setup() {
-  createCanvas(800, 500);
-  }
-  function draw() {
-  if (mouseIsPressed) {
-    c = color(random(0,255), random())
-  fill(0);
-  } 
-  else {
-  fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-  }
+  createCanvas(710, 400);
+  strokeWeight(20.0);
+  stroke(100, 200);
+  
+}
+
+function draw() {
+  background(40);
+
+  dx = mouseX - x;
+  dy = mouseY - y;
+  angle1 = atan2(dy, dx);
+  x = mouseX - cos(angle1) * segLength;
+  y = mouseY - sin(angle1) * segLength;
+
+  segment(x, y, angle1);
+  ellipse(x, y, 40, 40);
+}
+
+function segment(x, y, a) {
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
+}
