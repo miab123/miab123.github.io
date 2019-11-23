b577dd6eb9db6d2faa2aafa4e4a8b82eab59eb7e
Mia Baker <mia77baker@gmail.com>
2019-11-23 09:28:53 -0800

    more js

--- CS160/processing/third_project/sketch.js
+++ CS160/processing/third_project/sketch.js
@@ -13,16 +13,16 @@ function draw() {
   for (let x = 0; x <= width; x = x + 10) {
     for (let y = 0; y <= height; y = y + 40) {
       // starting point of each circle depends on mouse position
-      const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-      const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
+      const xAngle = map(mouseX, 0, width, -8 * PI, 4 * PI, true);
+      const yAngle = map(mouseY, 0, height, -8 * PI, 4 * PI, true);
       // and also varies based on the particle's location
       const angle = xAngle * (x / width) + yAngle * (y / height);
 
       // each particle moves in a circle
-      const myX = x + 20 * cos(2 * PI * t + angle);
+      const myX = x + 10 * cos(2 * PI * t + angle);
       const myY = y + 20 * sin(2 * PI * t + angle);
 
-      ellipse(myX, myY, 10); // draw particle
+      ellipse(myX, myY, 5); // draw particle
     }
   }
 
